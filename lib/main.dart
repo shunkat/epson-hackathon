41b39003a73acc06d3657cfd97b1dec162746d50
hitose-A-kakazu <hi.ka1749@gmail.com>
2022-03-13 06:50:22 +0900

    デザイン変更１

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   // Fireabse初期化
+  // debugPaintSizeEnabled = true;
   await Firebase.initializeApp();
   runApp(MyApp());
 }
@@ -42,50 +43,82 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: OrientationBuilder(
         builder: (context, orientation) {
-          return Row(
-            children: <Widget>[
-              SizedBox(
-                width: 250, //横幅
-                height: 300, //高さ
-                child: ElevatedButton(
-                  child: Text("EpsonConncect未登録の方\n最初はこちら"),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Registration(),
-                        ));
-                  },
-                ),
-              ),
-              SizedBox(
-                width: 250, //横幅
-                height: 300, //高さ
-                child: ElevatedButton(
-                  child: Text(
-                    "EpsonConncect登録済みの方",
+          return Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: 230, //横幅
+                  height: 100, //高さ
+                  margin: EdgeInsets.only(
+                      top: 170, bottom: 30, right: 30, left: 30),
+                  decoration: BoxDecoration(
+                    // 枠線の装飾、背景色などの指定
+                    border:
+                        Border.all(color: Colors.black, width: 5), //　枠線の色と太さを指定
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 3.0,
+                        spreadRadius: 1.0,
+                        offset: Offset(5, 5),
+                      ),
+                    ],
+                    color: Colors.yellow[200], // 内部の色を指定
+                  ), // 内部の色を指定,
+                  child: ElevatedButton(
+                    child: Text(
+                      "EpsonConncect未登録の方\n最初はこちら",
+                      textAlign: TextAlign.center,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Registration(),
+                          ));
+                    },
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Registration(),
-                        ));
-                  },
                 ),
-                onPressed: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SendData(),
-                      )
-                  );
-                },
-              ),
-            )
-          ],
-        );
-      },
-    ),
+                Container(
+                  width: 230, //横幅
+                  height: 100, //高さ
+                  margin:
+                      EdgeInsets.only(top: 60, bottom: 30, right: 30, left: 30),
+                  decoration: BoxDecoration(
+                    // 枠線の装飾、背景色などの指定
+                    border:
+                        Border.all(color: Colors.black, width: 5), //　枠線の色と太さを指定
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 3.0,
+                        spreadRadius: 1.0,
+                        offset: Offset(5, 5),
+                      ),
+                    ],
+                    color: Colors.yellow[200], // 内部の色を指定
+                  ),
+                  child: ElevatedButton(
+                    child: Text(
+                      "EpsonConncect登録済みの方",
+                      textAlign: TextAlign.center,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SendData(),
+                          ));
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
