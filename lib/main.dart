c49e0140a548083ce04d94eb7ad1aac12370b422
hitose-A-kakazu <hi.ka1749@gmail.com>
2022-03-13 14:18:33 +0900

    UIDesign変更したお

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
         // fontFamily: "Noto Sans JP",
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -41,83 +41,132 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          return Center(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  width: 230, //横幅
-                  height: 100, //高さ
-                  margin: EdgeInsets.only(
-                      top: 170, bottom: 30, right: 30, left: 30),
-                  decoration: BoxDecoration(
-                    // 枠線の装飾、背景色などの指定
-                    border:
-                        Border.all(color: Colors.black, width: 5), //　枠線の色と太さを指定
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 3.0,
-                        spreadRadius: 1.0,
-                        offset: Offset(5, 5),
-                      ),
-                    ],
-                    color: Colors.yellow[200], // 内部の色を指定
-                  ), // 内部の色を指定,
-                  child: ElevatedButton(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: FractionalOffset.topLeft,
+            end: FractionalOffset.bottomRight,
+            colors: [
+              Color.fromARGB(255, 253, 95, 95).withOpacity(0.8),
+              Color.fromARGB(255, 129, 0, 221).withOpacity(0.8),
+            ],
+            stops: const [
+              0.0,
+              1.0,
+            ],
+          ),
+        ),
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: 230, //横幅
+                    height: 200, //高さ
+                    margin: EdgeInsets.only(
+                        top: 130, bottom: 80, right: 30, left: 30),
                     child: Text(
-                      "EpsonConncect未登録の方\n最初はこちら",
+                      "EpsonConnect登録\nまだ登録していない方はNo Registeredのボタン\n登録済みの方はAlready Registeredのボタン\nを押してください",
+                      // "Click the No Registered button if you are not yet registered with EpsonConnect, or click the Already Registered button if you are registered with EpsonConnect",
                       textAlign: TextAlign.center,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Registration(),
-                          ));
-                    },
-                  ),
-                ),
-                Container(
-                  width: 230, //横幅
-                  height: 100, //高さ
-                  margin:
-                      EdgeInsets.only(top: 60, bottom: 30, right: 30, left: 30),
-                  decoration: BoxDecoration(
-                    // 枠線の装飾、背景色などの指定
-                    border:
-                        Border.all(color: Colors.black, width: 5), //　枠線の色と太さを指定
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 3.0,
-                        spreadRadius: 1.0,
-                        offset: Offset(5, 5),
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 255, 255, 255),
                       ),
-                    ],
-                    color: Colors.yellow[200], // 内部の色を指定
-                  ),
-                  child: ElevatedButton(
-                    child: Text(
-                      "EpsonConncect登録済みの方",
-                      textAlign: TextAlign.center,
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SendData(),
-                          ));
-                    },
                   ),
-                ),
-              ],
-            ),
-          );
-        },
+                  Container(
+                    width: 180, //横幅
+                    height: 50, //高さ
+                    margin: EdgeInsets.only(
+                        top: 30, bottom: 30, right: 30, left: 30),
+                    decoration: BoxDecoration(
+                      // 枠線の装飾、背景色などの指定
+                      // color: Colors.white, // 内部の色を指定
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          width: 2), //　枠線の色と太さを指定
+
+                      // boxShadow: [
+                      //   BoxShadow(
+                      //     color: Colors.black26,
+                      //     blurRadius: 3.0,
+                      //     spreadRadius: 1.0,
+                      //     offset: Offset(5, 5),
+                      //   ),
+                      // ],
+                    ), // 内部の色を指定,
+                    child: ElevatedButton(
+                      child: Text(
+                        "Not registered",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          // fontSize: 16,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Registration(),
+                            ));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.transparent,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 180, //横幅
+                    height: 50, //高さ
+                    margin: EdgeInsets.only(
+                        top: 20, bottom: 30, right: 30, left: 30),
+                    decoration: BoxDecoration(
+                      // 枠線の装飾、背景色などの指定
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          width: 2), //　枠線の色と太さを指定
+
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.transparent,
+                          blurRadius: 3.0,
+                          spreadRadius: 1.0,
+                          offset: Offset(5, 5),
+                        ),
+                      ],
+                    ),
+                    child: ElevatedButton(
+                      child: Text(
+                        "Already registered",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SendData(),
+                            ));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary:
+                            // Color.fromARGB(255, 255, 155, 155).withOpacity(0.5),
+                            Colors.transparent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
